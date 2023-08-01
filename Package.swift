e360f6dc3b722060b388cf0a3a1c38fcb42f089f
Vasyl Morarash <vmorarash@screenmeet.com>
2023-08-01 14:18:16 +0300

    Add spm support

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UniversalWebRTC",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "WebRTC",
+                      path: "WebRTC.xcframework")
+        
+    ]
+)
--- README.md
+++ README.md
@@ -5,12 +5,23 @@ Bitcode disabled by default
 
 ## Installation
 
+### CocoaPods
 WebRTC is available through [CocoaPods](http://cocoapods.org). To install
 it, simply add the following line to your Podfile:
 
 ```ruby
 pod 'UniversalWebRTC'
 ```
+
+### Swift Package Manager
+Add the project as a dependency to your Package.swift:
+```swift
+dependencies: [
+    .package(url: "https://github.com/Rostyk/UniversalWebRTC", .upToNextMinor(from: "106.0.7"))
+]
+```
+
+Then import `import WebRTC`.
 
 ## Build Author
 
